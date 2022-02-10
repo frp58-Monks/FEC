0166dec5f5baf6940e5821e9aa47a3aac03199f5
Justin Greer <justin.t.greer1@gmail.com>
2022-02-10 10:50:11 -0800

    Dependencies & installs

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,39 @@
-/*
-Imports List:
-  -React
-  -Axios
-  -Styles
-  -Other React Components
-*/
+import React from 'react';
+import axios from 'axios';
+var API = './theAPI';
+import Token from '../config/config.js';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: ''
+    };
+    //------------Bind_Class_Methods_Here------------//
+    this.getProducts = this.getProducts.bind(this);
+  }
+  //------------Class_Methods------------//
+  getProducts(table) {
+    axios.get(API, {
+      authorization: Token,
+      params: table
+    })
+    .then((res) => {
+      this.seteState({input: res.data});
+    })
+    .catch((err) => {
+      console.log({'GET Req': err})
+    })
+  }
+
+  //------------Render_Here------------//
+  render () {
+    return (
+      <div className="content">
+        <div>Jello World</div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ client/src/config/example.config.js
@@ -0,0 +1,9 @@
+// Put your API key goes here!
+// module.exports = {
+//   Token: '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'
+// }
+
+// in App.jsx import Token from './config/config.js';
+
+const Token = 'Github_API_Token_Here';
+export default Token;
