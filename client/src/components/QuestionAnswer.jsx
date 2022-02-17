d25c3a882aea29392ddbc1b2a53da95fe31774be
SD3K <skipdawg3000@gmail.com>
2022-02-16 17:28:43 -0800

    building out QA component structure

--- client/src/components/QuestionAnswer.jsx
+++ client/src/components/QuestionAnswer.jsx
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 
 const QuestionAnswer = (props) => {
+
+  const [questions, setQuestions] = useState([]);
+  const [question_id, setQuestion_id] = useState(null);
+  const [product_id, setProduct_id] = useState(props.product_id);
+
+  useEffect(() => (
+    axios.get('/qa/questions', { params: { product_id, page: 1, count: 2 }})
+  ))
+
+
   return (
     <div>Question and Answer Components Here</div>
   );
